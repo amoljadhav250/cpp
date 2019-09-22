e8d56b1e8590b43a71110e9fe602fd8f35aabeb1
amoljadhav250 <amoljadhav250@gmail.com>
2019-09-22 13:00:52 +0530

    Update ValidIP.cpp

--- ValidIP.cpp
+++ ValidIP.cpp
@@ -8,48 +8,58 @@
 #define S second
 #define M 1000000007
 using namespace std;
-
+ 
 bool isValid(string s){
+	
 	if(s=="0"){
 		return true;
 	}else if(s.size()>1 && s[0]=='0'){
 		return false;
-	}else if(stoi(s)>1 && stoi(s)<=255){
+	}else if(stoi(s)>=1 && stoi(s)<=255){
 		return true;
 	}else{
 			return false;
 	}
 	return false;
 }
+ 
+vector<string> validIp(string A){
 
-string validIp(string a){
-	int count=0;
+	string a=A;
+	vector<string>v;
+	if(a.length()>12){
+		return v;
+	}
+//	int count=0;
 	int al=a.length();
 		for(int i=1;i<=3;i++){
 		for(int j=i+1;j<=i+3;j++){
 			for(int k=j+1;k<=j+3;k++){
-			//	cout<<i<<" "<<j<<" "<<k<<"\n";
-			//	cout<<count++<<"\n";
-			//	cout<<a.substr(0,i)<<" ";
-			//	cout<<a.substr(i,j-i)<<" ";
-			//	cout<<a.substr(j,k-j)<<" ";
-			//	cout<<a.substr(k,al-k)<<"\n";
+			//	cout<<++count<<" ";
+			if(k<al){
 			string x1,x2,x3,x4;
-			x1=a.substr(0,i);
-			x2=a.substr(i,j-i);
-			x3=a.substr(j,k-j);
+			x1=a.substr(0,min(i,al));
+			x2=a.substr(i,min(j-i,al-i));
+			x3=a.substr(j,min(k-j,al-j));
 			x4=a.substr(k,al-k);
-			
+ 
 			if(isValid(x1) && isValid(x2) && isValid(x3) && isValid(x4) ){
-				cout<<x1<<"."<<x2<<"."<<x3<<"."<<x4<<endl;
+				string v1=x1+"."+x2+"."+x3+"."+x4;
+			v.push_back(v1);
 			}
+			}
+ 
 			}
 		}
 	}
-	return "a";
+	sort(v.begin(),v.end());
+	return v;
 }
-
+ 
 int main(){
-	validIp("25525511135");
-
+	vector<string> k=validIp("010010");
+	for(int i=0;i<k.size();i++){
+		cout<<k[i]<<endl;
+	}
+ 
 }
