cf665eed92c680ba3577797b9706559c23a3e011
amoljadhav250 <amoljadhav250@gmail.com>
2020-02-10 09:03:41 +0530

    Create MinHeapUsingSTL.cpp

---
+++ MinHeapUsingSTL.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	priority_queue<int,vector<int>, greater<int> > pq;
+	pq.push(2);
+	pq.push(3);
+	pq.push(1);
+	pq.push(5);
+	pq.push(4);
+	cout<<pq.size()<<" ";
+	while(pq.empty()==false){
+		cout<<pq.top()<<" ";
+		pq.pop();
+	}
+	return 0;
+}
